58a31ff9a09a0dec3644c1330331131a235495b0
vanla22 <anhhvan2212@gmail.com>
2021-11-19 09:57:17 +0700

    Create nhanvien.java

---
+++ duan/src/main/java/com/duan/entity/nhanvien.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.duan.entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class nhanvien {
+     private String maNV;
+    private String matKhau;
+    private String hoTen;
+    private String ngaySinh;
+    private String sdt;
+    private boolean vaiTro;
+
+    public nhanvien() {
+    }
+
+    public nhanvien(String maNV, String matKhau, String hoTen, String ngaySinh, String sdt, boolean vaiTro) {
+        this.maNV = maNV;
+        this.matKhau = matKhau;
+        this.hoTen = hoTen;
+        this.ngaySinh = ngaySinh;
+        this.sdt = sdt;
+        this.vaiTro = vaiTro;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public boolean isVaiTro() {
+        return vaiTro;
+    }
+
+    public void setVaiTro(boolean vaiTro) {
+        this.vaiTro = vaiTro;
+    }
+    
+}
